7b833a739d19fcd157aa819f86f422e0ded7ccd1
juanchaves <chavesjuanm@gmail.com>
2017-04-01 21:24:36 -0700

    added player choice and game active bool

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,5 +1,32 @@
 '''
 Rock, Paper, Scissors
 '''
+import random
 
-print('Hell and welcome to the rock, paper, scissors game!')
+prompt_error = 'You did not select from the allowed options'
+
+print('Hello and welcome to the rock, paper, scissors game!\n')
+game_is_active = True
+
+while game_is_active:
+    options = ['rock', 'paper', 'scissors']
+    player_choice = input('Choose to throw rock, paper or scissors ')
+    player_choice.lower()
+    while player_choice not in options
+        print(prompt_error)
+        player_choice = input('Choose to throw: rock, paper or scissors ')
+        player_choice.lower()
+
+    computer_choice = random.choice(option)
+    print(computer_choice)
+
+    while type(game_is_active) is not bool:
+        game_is_active = input('Would you like to keep playing? (y/n) ')
+        game_is_active.lower()
+
+        if game_is_active == 'y' or if game_is_active == 'yes':
+            game_is_active = True
+        elif game_is_active == 'n' or if game_is_active == 'no':
+            game_is_active = False
+        else:
+            print(prompt_error)
